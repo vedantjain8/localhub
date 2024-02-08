9d3c57e3a74275b8cd33f6397cba21bbd4f50aa6
bhavyaj19 <bhavyaj525@gmail.com>
2024-02-08 20:15:11 +0530

    feat: added image picker

---
+++ flutter/localhub/lib/screens/layout/create_post.dart
@@ -0,0 +1,131 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:localhub/widgets/custom_text_field_input.dart';
+
+class CreatePost extends StatefulWidget {
+  const CreatePost({super.key});
+
+  @override
+  State<CreatePost> createState() => _CreatePostState();
+}
+
+class _CreatePostState extends State<CreatePost> {
+  TextEditingController _postTitleController = TextEditingController();
+  TextEditingController _postDescriptionController = TextEditingController();
+
+  XFile? pickedImage;
+  final _picker = ImagePicker();
+
+  Future<void> _openGallery() async {
+    Navigator.of(context).pop();
+    pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {});
+    }
+  }
+
+  Future<void> _openCamera() async {
+    Navigator.of(context).pop();
+    pickedImage = await _picker.pickImage(source: ImageSource.camera);
+    if (pickedImage != null) {
+      setState(() {});
+    }
+    // Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _postTitleController.dispose();
+    _postDescriptionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Post'),
+        actions: [
+          ElevatedButton(onPressed: () {}, child: const Text('Next')),
+          const SizedBox(width: 15)
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: pickedImage != null
+          ? null
+          : FloatingActionButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.15,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            IconButton.filled(
+                              onPressed: _openGallery,
+                              icon: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: FaIcon(FontAwesomeIcons.image),
+                              ),
+                              tooltip: 'Choose from gallery',
+                            ),
+                            IconButton.filled(
+                              onPressed: _openCamera,
+                              icon: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: FaIcon(FontAwesomeIcons.camera),
+                              ),
+                              tooltip: 'click a image',
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              },
+              shape: const CircleBorder(),
+              child: const FaIcon(FontAwesomeIcons.upload),
+            ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextFieldInput(
+                hasPrefix: false,
+                textEditingController: _postTitleController,
+                textInputType: TextInputType.text,
+                prefixIcon: const Icon(Icons.short_text_rounded),
+                label: 'Title',
+                hintText: '',
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              CustomTextFieldInput(
+                hasPrefix: false,
+                textEditingController: _postDescriptionController,
+                hintText: '(optional)',
+                textInputType: TextInputType.text,
+                prefixIcon: const Icon(Icons.notes_rounded),
+                maxLines: 10,
+                label: 'Description',
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              pickedImage == null
+                  ? Container()
+                  : Image.file(File(pickedImage!.path))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
