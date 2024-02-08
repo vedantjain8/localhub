28a98e9d968163d82cbecfb88f9af04db2716293
bhavyaj19 <bhavyaj525@gmail.com>
2024-02-08 20:23:41 +0530

    chore: dependencies updated

--- flutter/localhub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/localhub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
 import flutter_secure_storage_macos
 import package_info_plus
 import path_provider_foundation
@@ -13,6 +14,7 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
