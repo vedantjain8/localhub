39b3bf719c6efe9ae59b371d2e53132c7408da9e
bhavyaj19 <bhavyaj525@gmail.com>
2024-02-16 00:02:43 +0530

    fix: replaced CustomTextFieldInput  to CustomInputDecoration

---
+++ flutter/localhub/lib/widgets/custom_input_decoration.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomInputDecoration {
+  static InputDecoration inputDecoration({
+    hintText = '',
+    label,
+    prefixIcon,
+    context,
+  }) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final inputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20.0),
+      borderSide: BorderSide(
+        color: colorScheme.onSurfaceVariant,
+      ),
+    );
+
+    return InputDecoration(
+        hintText: hintText,
+        label: Text(label),
+        hintStyle: TextStyle(
+          color: colorScheme.onSurfaceVariant,
+        ),
+        border: inputBorder,
+        focusedBorder: inputBorder,
+        prefixIcon: prefixIcon
+        // prefixIcon: Icon(hasPrefix == false ? null : prefixIcon),
+        );
+  }
+}
