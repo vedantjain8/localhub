d9889f685ffc1bb9e8d22b57263b7f3fe3ed7528
bhavyaj19 <bhavyaj525@gmail.com>
2024-03-08 22:02:12 +0530

    fix: settings screen ui

--- flutter/localhub/lib/screens/layout/settings/settings_screen.dart
+++ flutter/localhub/lib/screens/layout/settings/settings_screen.dart
@@ -1,10 +1,11 @@
 // todo: theme, hostaddress and submit, main.dart me server down aur update wali screen pe
 
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:localhub/api/version_check.dart';
 import 'package:localhub/themes/theme.dart';
-import 'package:localhub/widgets/custom_input_decoration.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -126,142 +127,246 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                _section(title: 'Theme Settings', items: [
-                  // TODO: fix  this item when error message pops up
-                  _sectionItem(
-                    child: TextField(
-                      controller: _hostAddressController,
-                      decoration: CustomInputDecoration.inputDecoration(
-                        context: context, label: ("HostAddress"),
-                        errorText: _submited ? _errorText : null,
-                        // hintText: 'hostaddress',
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _section(
+                    title: 'App Settings',
+                    items: [
+                      const SizedBox(
+                        height: 10,
                       ),
-                    ),
-                  ),
-                  _sectionItem(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _checkUrl();
-                      },
-                      child: const Icon(Icons.save_rounded),
-                    ),
-                  ),
-                  _sectionItem(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Theme',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        Transform.scale(
-                          scale: 1.3,
-                          child: Switch(
-                            trackOutlineColor: MaterialStatePropertyAll(
-                                colorScheme.secondaryContainer),
-                            inactiveTrackColor: colorScheme.secondaryContainer,
-                            activeTrackColor: colorScheme.secondaryContainer,
-                            thumbColor:
-                                MaterialStatePropertyAll(colorScheme.secondary),
-                            value: isDark,
-                            onChanged: (value) async {
-                              setState(() {
-                                isDark = !isDark;
-                              });
-                              await AppTheme.toggleBrightness();
-                            },
-                            thumbIcon: MaterialStatePropertyAll(
-                              isDark
-                                  ? Icon(
-                                      FontAwesomeIcons.solidSun,
-                                      color: colorScheme.onInverseSurface,
-                                    )
-                                  : Icon(
-                                      FontAwesomeIcons.solidMoon,
-                                      color: colorScheme.onInverseSurface,
-                                    ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const Divider(),
-                  _sectionItem(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Colors',
-                          style: Theme.of(context).textTheme.titleLarge,
-                        ),
-                        Row(
+                      _sectionItem(
+                        height: _submited ? 100 : 80,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            DropdownButton<Color>(
-                              value: AppTheme.currentColorSeed.color,
-                              icon: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Icon(
-                                  FontAwesomeIcons.angleDown,
-                                  color: colorScheme.secondary,
-                                ),
-                              ),
-                              items: ColorSeed.values.map((colorSeed) {
-                                return DropdownMenuItem(
-                                  value: colorSeed.color,
-                                  child: Row(
-                                    children: [
-                                      CircleAvatar(
-                                        radius: 10,
-                                        backgroundColor: colorSeed.color,
+                            Text(
+                              'HostAddress',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: TextField(
+                                controller: _hostAddressController,
+                                decoration: InputDecoration(
+                                    // label: const Text("HostAddress"),
+                                    errorText: _submited ? _errorText : null,
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      borderSide: BorderSide(
+                                        color: colorScheme.outlineVariant,
                                       ),
-                                      const SizedBox(width: 10),
-                                      Text(colorSeed.label),
-                                    ],
-                                  ),
-                                );
-                              }).toList(),
-                              onChanged: (color) {
-                                AppTheme.selectColor(color!);
-                              },
+                                    ),
+                                    suffix: IconButton(
+                                        onPressed: () {
+                                          _checkUrl();
+                                        },
+                                        icon: const Icon(
+                                            FontAwesomeIcons.solidFloppyDisk)),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      borderSide: BorderSide(
+                                        color: colorScheme.outlineVariant,
+                                      ),
+                                    )),
+                              ),
                             ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      // const Divider(),
+                      // _sectionItem(
+                      //   child: ElevatedButton(
+                      //     onPressed: () {
+                      //       _checkUrl();
+                      //     },
+                      //     child: const Icon(Icons.save_rounded),
+                      //   ),
+                      // ),
+                      _sectionItem(
+                        height: 60,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Theme',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            Switch(
+                              trackOutlineColor: MaterialStatePropertyAll(
+                                  colorScheme.secondaryContainer),
+                              inactiveTrackColor:
+                                  colorScheme.secondaryContainer,
+                              activeTrackColor: colorScheme.secondaryContainer,
+                              thumbColor: MaterialStatePropertyAll(
+                                  colorScheme.secondary),
+                              value: isDark,
+                              onChanged: (value) async {
+                                setState(() {
+                                  isDark = !isDark;
+                                });
+                                await AppTheme.toggleBrightness();
+                              },
+                              thumbIcon: MaterialStatePropertyAll(
+                                isDark
+                                    ? Icon(
+                                        FontAwesomeIcons.solidSun,
+                                        color: colorScheme.onInverseSurface,
+                                      )
+                                    : Icon(
+                                        FontAwesomeIcons.solidMoon,
+                                        color: colorScheme.onInverseSurface,
+                                      ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      // const Divider(),
+                      _sectionItem(
+                        height: 70,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Colors',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    border: Border.all(
+                                        color: colorScheme.outlineVariant,
+                                        width: 1),
+                                  ),
+                                  child: DropdownButton<Color>(
+                                    padding: const EdgeInsets.all(10),
+                                    borderRadius: BorderRadius.circular(20),
+                                    underline: Container(),
+                                    value: AppTheme.currentColorSeed.color,
+                                    icon: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Icon(
+                                        FontAwesomeIcons.angleDown,
+                                        color: colorScheme.secondary,
+                                      ),
+                                    ),
+                                    items: ColorSeed.values.map((colorSeed) {
+                                      return DropdownMenuItem(
+                                        value: colorSeed.color,
+                                        child: Row(
+                                          children: [
+                                            CircleAvatar(
+                                              radius: 10,
+                                              backgroundColor: colorSeed.color,
+                                            ),
+                                            const SizedBox(width: 10),
+                                            Text(colorSeed.label),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList(),
+                                    onChanged: (color) {
+                                      AppTheme.selectColor(color!);
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ]),
-              ],
-            ),
-          ],
+                  _section(title: "About", items: [
+                    InkWell(
+                      child: _sectionItem(
+                        child: Text(
+                          'Content Policy',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        height: 50,
+                      ),
+                    ),
+                    InkWell(
+                      child: _sectionItem(
+                        child: Text(
+                          'Privacy Policy',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        height: 50,
+                      ),
+                    ),
+                    InkWell(
+                      child: _sectionItem(
+                        child: Text(
+                          'User Agreement',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        height: 50,
+                      ),
+                    ),
+                    InkWell(
+                      child: _sectionItem(
+                        child: Text(
+                          'Acknowledgements',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        height: 50,
+                      ),
+                    ),
+                  ])
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _section({title, final List<Widget>? items}) {
-    return Column(
-      children: [
-        if (title != null) Text(title),
-        if (items != null)
-          Column(
-            children: items,
-          )
-      ],
+  Widget _section({String? title, final List<Widget>? items}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (title != null)
+            Text(
+              title,
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          if (items != null)
+            Column(
+              children: items,
+            ),
+          const Divider()
+        ],
+      ),
     );
   }
 
-  Widget _sectionItem({required child}) {
-    return SizedBox(
-      width: double.maxFinite,
-      height: 70,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: child,
+  Widget _sectionItem({required child, required double height}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: SizedBox(
+        width: double.maxFinite,
+        height: height,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: child,
+        ),
       ),
     );
   }
