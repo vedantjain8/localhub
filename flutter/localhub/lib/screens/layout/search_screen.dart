cf6a46571126920477e320fda8497591f7f68934
vedantjain8 <vedantjain88666@gmail.com>
2024-03-08 14:03:34 +0530

    feat: search screen for post and community search refactor: app version update

--- flutter/localhub/lib/screens/layout/search_screen.dart
+++ flutter/localhub/lib/screens/layout/search_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:localhub/api/community_service.dart';
+import 'package:localhub/api/search_service.dart';
 import 'package:localhub/screens/community/community_page.dart';
+import 'package:localhub/screens/post/post_page.dart';
 
 class Debouncer {
   Debouncer({required this.milliseconds});
@@ -26,72 +27,142 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final SearchController _searchController = SearchController();
   final _debouncer = Debouncer(milliseconds: 500);
-  final CommunityApiService cas = CommunityApiService();
+  final SearchApiService sas = SearchApiService();
 
-  List<Map<String, dynamic>> searchList = [];
+  Map<String, dynamic> searchList = {};
 
   void _loadSearchData(String searchControllerText) {
-    if (searchControllerText.isNotEmpty) {
-      _debouncer.run(() async {
-        List<Map<String, dynamic>> data =
-            await cas.getCommunityList(communityName: searchControllerText);
-        setState(() {
-          searchList = data;
-        });
+    // if (searchControllerText.isNotEmpty) {
+    _debouncer.run(() async {
+      Map<String, dynamic> data =
+          await sas.search(searchTerm: searchControllerText.toString());
+      setState(() {
+        searchList = data['response'];
       });
-    }
+    });
+    // }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _loadSearchData('');
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 50.0, left: 10, right: 10),
-        child: Column(
-          children: [
-            TextField(
-              controller: _searchController,
-              onChanged: (String value) {
-                _loadSearchData(value);
-              },
-              decoration: const InputDecoration(
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _searchController,
+                onChanged: _loadSearchData,
+                decoration: const InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: searchList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onTap: () => Navigator.of(context).push(
+              const SizedBox(height: 40),
+              buildCommunitySearchResult(
+                  'Community', searchList['communityData']),
+              const SizedBox(height: 40),
+              buildPostSearchResult('Posts', searchList['postData']),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCommunitySearchResult(String title, List<dynamic>? data) {
+    if (data == null || data.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 10),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              data.length,
+              (index) => Padding(
+                padding: const EdgeInsets.only(right: 15, left: 15),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
                       MaterialPageRoute(
                         builder: (context) => CommunityPage(
-                            communityID: searchList[index]['community_id']),
+                            communityID: data[index]['community_id']),
                       ),
-                    ),
-                    child: ListTile(
-                      leading: Container(
-                        height: 33,
-                        width: 33,
+                    );
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 80,
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
                           image: DecorationImage(
                             image: CachedNetworkImageProvider(
-                              searchList[index]['logo_url'],
-                            ),
+                                data[index]["logo_url"]),
+                            fit: BoxFit.cover,
                           ),
                         ),
                       ),
-                      title: Text(searchList[index]['community_name']),
-                    ),
-                  );
-                },
+                      Text(data[index]["community_name"]),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ],
+          ),
         ),
-      ),
+      ],
+    );
+  }
+
+  Widget buildPostSearchResult(String title, List<dynamic>? data) {
+    if (data == null || data.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(data[index]['post_title']),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        PostPage(postID: data[index]['post_id'])));
+              },
+            );
+          },
+        ),
+      ],
     );
   }
 }
