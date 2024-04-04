6135eca8a79489f082f598fae72b96f8fbbc08ae
bhavyaj19 <bhavyaj525@gmail.com>
2024-04-04 11:44:53 +0530

    feat: date format

--- flutter/localhub/lib/functions/datetimeoperations.dart
+++ flutter/localhub/lib/functions/datetimeoperations.dart
@@ -1,28 +1,37 @@
-String timeAgo(String fetchedDateString) {
-  DateTime fetchedDate = DateTime.parse(fetchedDateString);
+import 'package:intl/intl.dart';
+
+String timeAgo(String fatchedDateString) {
+  DateTime fetchedDate = DateTime.parse(fatchedDateString);
   DateTime currentDate = DateTime.now();
 
   var different = currentDate.difference(fetchedDate);
 
   if (different.inDays > 365) {
-    return "${(different.inDays / 365).floor()}y ";
+    return "${(different.inDays / 365).floor()}y";
   }
   if (different.inDays > 30) {
-    return "${(different.inDays / 30).floor()}m ";
+    return "${(different.inDays / 30).floor()}m";
   }
   if (different.inDays > 7) {
-    return "${(different.inDays / 7).floor()}w ";
+    return "${(different.inDays / 7).floor()}w";
   }
   if (different.inDays > 0) {
-    return "${different.inDays}d ";
+    return "${different.inDays}d";
   }
   if (different.inHours > 0) {
-    return "${different.inHours}hr ";
+    return "${different.inHours}hr";
   }
   if (different.inMinutes > 0) {
-    return "${different.inMinutes}min ";
+    return "${different.inMinutes}min";
   }
   if (different.inMinutes == 0) return 'Just Now';
 
   return fetchedDate.toString();
+}
+
+String dateFormat(String fatchedDateString) {
+  DateTime fetchedDate = DateTime.parse(fatchedDateString);
+  String formattedDate = "";
+  formattedDate = DateFormat.yMMMd().format(fetchedDate);
+  return formattedDate;
 }
