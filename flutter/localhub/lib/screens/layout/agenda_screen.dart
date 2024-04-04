b00edb2fcd0533ac1eb48969febad89f0b21d581
bhavyaj19 <bhavyaj525@gmail.com>
2024-04-04 11:23:13 +0530

    fix: ui

--- flutter/localhub/lib/screens/layout/agenda_screen.dart
+++ flutter/localhub/lib/screens/layout/agenda_screen.dart
@@ -1,5 +1,7 @@
 import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter/material.dart";
+import "package:localhub/functions/datetimeoperations.dart";
+import "package:localhub/widgets/custom_shimmer.dart";
 
 class AgendaScreen extends StatefulWidget {
   final List<Map<String, dynamic>> agendaList;
@@ -13,54 +15,109 @@ class _AgendaScreenState extends State<AgendaScreen> {
   List<Map<String, dynamic>> agendaList = [];
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     agendaList = widget.agendaList;
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: agendaList.length,
-      itemBuilder: ((context, index) {
-        return Container(
-          padding: const EdgeInsets.all(4),
-          child: Column(
-            children: [
-              Visibility(
-                visible: agendaList[index]["image_url"] != null &&
-                    agendaList[index]["image_url"]!.isNotEmpty,
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return (agendaList.isEmpty)
+        ? const SingleChildScrollView(
+            child: CustomShimmer(),
+          )
+        : ListView.builder(
+            shrinkWrap: true,
+            itemCount: agendaList.length,
+            itemBuilder: ((context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
                 child: Container(
-                  width: double.maxFinite,
-                  constraints: const BoxConstraints(
-                    maxHeight: 500,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: colorScheme.onInverseSurface,
                   ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: CachedNetworkImage(
-                      fit: BoxFit.fitWidth,
-                      imageUrl: agendaList[index]["image_url"],
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) => Center(
-                        child: CircularProgressIndicator(
-                            value: downloadProgress.progress),
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 210,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                "${dateFormat(agendaList[index]["agenda_start_date"])} - ${dateFormat(agendaList[index]["agenda_end_date"])}"),
+                            const SizedBox(height: 10),
+                            Text(
+                              agendaList[index]["agenda_title"],
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.start,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              agendaList[index]["agenda_description"],
+                              style: Theme.of(context).textTheme.titleMedium,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                Text(
+                                  agendaList[index]["locality_city"] + ", ",
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                  textAlign: TextAlign.start,
+                                ),
+                                Text(
+                                  agendaList[index]["locality_state"] + ", ",
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                  textAlign: TextAlign.start,
+                                ),
+                                Text(
+                                  agendaList[index]["locality_country"],
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                  textAlign: TextAlign.start,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 5),
+                          ],
+                        ),
                       ),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    ),
+                      const SizedBox(width: 6),
+                      Visibility(
+                        visible: agendaList[index]["image_url"] != null &&
+                            agendaList[index]["image_url"]!.isNotEmpty,
+                        child: SizedBox(
+                          height: 130,
+                          width: 130,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: CachedNetworkImage(
+                              fit: BoxFit.cover,
+                              imageUrl: agendaList[index]["image_url"],
+                              progressIndicatorBuilder:
+                                  (context, url, downloadProgress) => Center(
+                                child: CircularProgressIndicator(
+                                    value: downloadProgress.progress),
+                              ),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Text(agendaList[index]["agenda_title"]),
-              Text(agendaList[index]["agenda_description"]),
-              Text(agendaList[index]["locality_city"]),
-              Text(agendaList[index]["locality_state"]),
-              Text(agendaList[index]["locality_country"]),
-            ],
-          ),
-        );
-      }),
-    );
+              );
+            }),
+          );
   }
 }
